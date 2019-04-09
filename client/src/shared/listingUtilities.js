8158ff40d0cb507c45441c102f1d889f52039567
Chad Lynch <chad@sentigence.com>
2019-04-09 19:19:23 +0800

    ran npm init

--- client/src/shared/listingUtilities.js
+++ client/src/shared/listingUtilities.js
@@ -1,7 +1,7 @@
 import * as contracts from "truffle-contract";
 import {web3Injected, currentProvider, activeUser} from "./metamaskUtils";
 
-const definition = require('./contracts/Listing');
+const definition = require('./contracts/Listing.json');
 const listingAddress = '0x988C0f9F747bD95C987D88205A33AB495235E9F4';
 
 export const deployCommitment = (successBeneficiary, failureBeneficiary, referee, title, description, deadline) => {
