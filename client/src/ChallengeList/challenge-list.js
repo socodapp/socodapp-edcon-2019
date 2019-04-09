569a55551b714f46aac2810bf49acffa7040e85f
seed <seed@users.noreply.github.com>
2019-04-09 10:53:34 +1000

    Fetch listing from etherscan and decode ABI.

--- client/src/ChallengeList/challenge-list.js
+++ client/src/ChallengeList/challenge-list.js
@@ -12,10 +12,16 @@ import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import CommentIcon from '@material-ui/icons/Comment';
 
+import { ethers } from 'ethers';
+
+const registryAddr = "0x223f38d48dA35f904D13Ce7E969353cBF0A6267E";
+const apiKey = "YourApiKeyToken";
+const apiUrl = "https://api-kovan.etherscan.io/api?module=logs&action=getLogs&fromBlock=10706350&toBlock=latest";
 
 class ChallengeList extends Component {
   state = {
     checked: [0],
+    items: [],
   };
 
   handleToggle = value => () => {
@@ -34,18 +40,58 @@ class ChallengeList extends Component {
     });
   };
 
+  decodeEventData = (data) => {
+    return ethers.utils.defaultAbiCoder.decode(["address", "address", "address", "address", "address", "string", "string", "uint256"], data)
+  }
+
+  componentDidMount() {
+    const restUrl = apiUrl + "&address=" + registryAddr + "&apikey=" + apiKey;
+    console.log(restUrl);
+    fetch(restUrl)
+      .then(res => res.json())
+     // TODO reverse ens all addresses in res.result
+      .then(
+        (res) => {
+          this.setState({
+            isLoaded: true,
+            items: res.result.map(item =>
+                [item.address].concat(this.decodeEventData(item.data)))
+          });
+          console.log(this.state.items);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
 
+    // TODO write a nice UI
+    const items = this.state.items;
     return (
       <List className={styles.root}>
-        {[0, 1, 2, 3].map(value => (
-          <ListItem key={value} role={undefined} dense button onClick={this.handleToggle(value)}>
+        {items.map(arr => (
+          <ListItem key={arr[0]} role={undefined} dense button onClick={this.handleToggle(arr[0])}>
             <Checkbox
-              checked={this.state.checked.indexOf(value) !== -1}
+              checked={this.state.checked.indexOf(arr[0]) !== -1}
               tabIndex={-1}
               disableRipple
             />
-            <ListItemText primary={`Line item ${value + 1}`} />
+            <ListItemText primary={`Contract Address ${arr[0]}`} />
+            <ListItemText primary={`Challenger ${arr[1]}`} />
+            <ListItemText primary={`Success Beneficiary ${arr[2]}`} />
+            <ListItemText primary={`Failure Beneficiary ${arr[3]}`} />
+            <ListItemText primary={`Referee ${arr[4]}`} />
+            <ListItemText primary={`Title ${arr[5]}`} />
+            <ListItemText primary={`Description ${arr[6]}`} />
+            <ListItemText primary={`Deadline ${arr[7]}`} />
             <ListItemSecondaryAction>
               <IconButton aria-label="Comments">
                 <CommentIcon />
