5aa4461534c30ae12ce659c5cf085b2295ea773f
Nikesh Nazareth <business@nikeshnazareth.com>
2019-04-10 01:23:37 +1000

    Add contractAddresses function to listingUtils

--- client/src/shared/listingUtilities.js
+++ client/src/shared/listingUtilities.js
@@ -2,7 +2,25 @@ import * as contracts from "truffle-contract";
 import {web3Injected, currentProvider, activeUser} from "./metamaskUtils";
 
 const definition = require('./contracts/Listing.json');
-const listingAddress = '0x023933Cb2E5Bc4cca75832730A37CA5Da6c28745';
+const listingAddress = '0x34A9706EEc2540A4e4606FeF7b8535A7CFfeE899';
+// const listingAddress = '0x023933Cb2E5Bc4cca75832730A37CA5Da6c28745';
+
+export const contractAddresses = () => {
+    if (web3Injected()) {
+        const Listing = contracts(definition);
+        Listing.setProvider(currentProvider());
+        return Listing.at(listingAddress)
+            .then(async (contract) => {
+                const N = (await contract.numberOfCommitments.call()).toNumber();
+                return Promise.all(
+                    Array(N).fill(0).map((_, i) => i)
+                        .map(i => contract.commitmentContracts.call(i))
+                )
+            })
+    } else {
+        return Promise.resolve([])
+    }
+};
 
 export const deployCommitment = (successBeneficiary, failureBeneficiary, referee, title, description, deadline) => {
     if (web3Injected()) {
@@ -16,7 +34,7 @@ export const deployCommitment = (successBeneficiary, failureBeneficiary, referee
                 title,
                 description,
                 deadline,
-                { from: activeUser() }
+                {from: activeUser()}
             ));
     }
 };
