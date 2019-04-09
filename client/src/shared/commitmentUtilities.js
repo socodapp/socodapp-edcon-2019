a111d32a222880696572244d6b1277d5d5791a8e
Nikesh Nazareth <business@nikeshnazareth.com>
2019-04-09 20:57:19 +1000

    Create utility to transfer DAI to contract

--- client/src/shared/commitmentUtilities.js
+++ client/src/shared/commitmentUtilities.js
@@ -2,6 +2,9 @@ import * as contracts from "truffle-contract";
 import {web3Injected, currentProvider, activeUser} from "./metamaskUtils";
 
 const definition = require('./contracts/SocialCommitment');
+const ERC20Definition = require('./contracts/SampleERC20');
+
+const DAI_ROPSTEN = '0x7dddbD7D0AEB365D90A6A8AF1fC33c95f47a4f84';
 
 export const isReferee = (addr) => {
     if (web3Injected()) {
@@ -12,4 +15,24 @@ export const isReferee = (addr) => {
             .then(referee => referee.toLowerCase() === activeUser().toLowerCase())
     }
     return Promise.resolve(false);
+};
+
+// Step 1 in a pledge
+export const assignDaiToContract = (addr, amount) => {
+    if (web3Injected()) {
+        const DAI = contracts(ERC20Definition);
+        DAI.setProvider(currentProvider());
+        DAI.at(DAI_ROPSTEN)
+            .then(contract => contract.approve(addr, amount, {from: activeUser()}))
+    }
+};
+
+// Step 2 in a pledge
+export const transferDaiToContract = (addr) => {
+    if (web3Injected()) {
+        const Commitment = contracts(definition);
+        Commitment.setProvider(currentProvider());
+        return Commitment.at(addr)
+            .then(contract => contract.pledge({from: activeUser()}))
+    }
 };
