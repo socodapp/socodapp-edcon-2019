9fc6d0bfac5d6e03dd872f87de33b0524c027566
seed <seed@users.noreply.github.com>
2019-04-09 14:26:35 +1000

    Update rest URL construction.

--- client/src/shared/etherscanUtils.js
+++ client/src/shared/etherscanUtils.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const registryAddr = "0x6fff4185512B1a9E2bab8461Be1CCCb625A62064";
 const apiKey = "YourApiKeyToken";
-const apiUrl = "https://api-kovan.etherscan.io/api"
-const logParams = "?module=logs&action=getLogs&fromBlock=10706350&toBlock=latest";
+const apiUrl = "https://api-kovan.etherscan.io/api" + "?apikey=" + apiKey
+const logParams = "&module=logs&action=getLogs&fromBlock=10706350&toBlock=latest";
 
 
 async function reqJSON(url) {
@@ -20,10 +20,9 @@ var decodeSocialCommitmentCreated = (data) => {
   return ethers.utils.defaultAbiCoder.decode(["address", "address", "address", "address", "address", "string", "string", "uint256"], data)
 }
 
-
 async function getListing() {
   try {
-    const restUrl = apiUrl + logParams + "&address=" + registryAddr + "&apikey=" + apiKey;
+    const restUrl = apiUrl + logParams + "&address=" + registryAddr;
     const items = await reqJSON(restUrl);
     return items.map(function (item) {
               const evArgs = decodeSocialCommitmentCreated(item.data);
